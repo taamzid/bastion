50fa70567124ca4bdf4eed74b8f6d1d4c6c14be3
zayed utsho <zayedutsho@example.com>
2023-12-13 01:12:52 +0600

    picture changed

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,19 +1,14 @@
 import "./navBar.css";
 import vastionLogo from "../../assets/VASTION-PW-GREEN 1.svg";
-import vastionLogoMobile from "../../assets/navNewLogo.svg";
+// import vastionLogoMobile from "../../assets/navNewLogo.svg";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const isSmallScreen = window.innerWidth <= 767;
   return (
     <>
       <div className="__nav-container">
         <Link to="/">
-          {isSmallScreen ? (
-            <img src={vastionLogoMobile} alt="vastionLogo" />
-          ) : (
-            <img src={vastionLogo} alt="vastionLogo" />
-          )}
+          <img src={vastionLogo} alt="vastionLogo" />
         </Link>
       </div>
       <div className="__line"></div>
